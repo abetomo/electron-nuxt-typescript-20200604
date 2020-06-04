4473ebba85b22874ab3abb9be9160e73d23e8d64
abetomo <abe@enzou.tokyo>
2020-06-04 18:27:00 +0900

    Add an example of using fs module

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,46 +1,70 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card title="Free" icon="github-circle">
-        Open source on
-        <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
+  <div>
+    <section class="section">
+      <div class="columns is-mobile">
+        <card title="Free" icon="github-circle">
+          Open source on
+          <a href="https://github.com/buefy/buefy">
+            GitHub
+          </a>
+        </card>
 
-      <card title="Responsive" icon="cellphone-link">
-        <b class="has-text-grey">
-          Every
-        </b>
-        component is responsive
-      </card>
+        <card title="Responsive" icon="cellphone-link">
+          <b class="has-text-grey">
+            Every
+          </b>
+          component is responsive
+        </card>
 
-      <card title="Modern" icon="alert-decagram">
-        Built with
-        <a href="https://vuejs.org/">
-          Vue.js
-        </a>
-        and
-        <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
+        <card title="Modern" icon="alert-decagram">
+          Built with
+          <a href="https://vuejs.org/">
+            Vue.js
+          </a>
+          and
+          <a href="http://bulma.io/">
+            Bulma
+          </a>
+        </card>
 
-      <card title="Lightweight" icon="arrange-bring-to-front">
-        No other internal dependency
-      </card>
-    </div>
-  </section>
+        <card title="Lightweight" icon="arrange-bring-to-front">
+          No other internal dependency
+        </card>
+      </div>
+    </section>
+    <section class="section">
+      <b-button @click="outputLog">LOG</b-button>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
 import Card from '~/components/Card.vue'
+
+declare global {
+  interface Window {
+    electron: {
+      remote: {
+        require: Function
+      }
+      logPath: string
+    }
+  }
+}
+
+const fs = window.electron.remote.require('fs')
 
 export default {
   name: 'HomePage',
 
   components: {
     Card
+  },
+
+  methods: {
+    outputLog() {
+      fs.appendFileSync(window.electron.logPath, Date.now() + '\n')
+    }
   }
 }
 </script>
---
+++ example/preload.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const { contextBridge, app, remote } = require('electron')
+
+const userDataPath = (() => {
+  try {
+    return app.getPath('userData')
+  } catch {}
+  try {
+    return remote.app.getPath('userData')
+  } catch {}
+  return ''
+})()
+
+contextBridge.exposeInMainWorld('electron', {
+  logPath: path.join(userDataPath, 'log.txt'),
+  remote
+})
